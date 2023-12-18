75c09ab48b9a78d2cb107f49aa8d7a8425660577
Hevelyn <hevelynnthais@gmail.com>
2023-12-18 18:13:28 -0300

    First commit
    First commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿//Exercício 1.
+//int x = int.Parse(Console.ReadLine());
+
+//for (int i = 1; i <= x; i++)
+//{
+//    if (i % 2 != 0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+
+
+//Exercício 2.
+//int N = int.Parse(Console.ReadLine());
+
+//if (N < 0)
+//{
+//    Console.WriteLine("NEGATIVO");
+//}
+//else
+//{
+//    Console.WriteLine("NAO NEGATIVO");
+//}
+
+
+
+
+//string pessoa1 = "Dados da primeira pessoa: ";
+
+//string pessoa2 = "Dados da segunda pessoa: ";
+
+
+//Console.WriteLine(pessoa1);
+//Console.Write("Nome: ");
+//Console.ReadLine();
+//Console.Write("Idade: ");
+//Console.ReadLine();
+
+//Console.WriteLine(pessoa2);
+//Console.Write("Nome: ");
+//Console.ReadLine();
+//Console.Write("Idade: ");
+//Console.ReadLine();
+
+
+//if (true)
+//{
+
+//}
